429f510ef7bdcf66956c9eda257a401e53965765
dylna <devdylancrowder@outlook.com>
2024-12-10 11:27:35 -0300

    1

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ app.use(express.json());
 
 // Define el endpoint
 app.get('/api/hello', (req, res) => {
-    res.status(200).json({ message: 'si anda mains' });
+    res.status(200).json({ message: 'provando' });
 });
 
 export default app;
